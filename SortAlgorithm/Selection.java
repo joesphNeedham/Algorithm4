c589d2974804e8a1724438be25943e87c578049d
tanbo <neil-1994@outlook.com>
2019-09-18 23:52:46 +0800

    修复排序算法中出现的问题，主要是缩进导致的视觉误差

--- SortAlgorithm/Selection.java
+++ SortAlgorithm/Selection.java
@@ -13,19 +13,26 @@ public class Selection {
             int min = i; //通过一个中间变量来，保存索引右边最小值的索引
             for(int j=i+1; j<N;j++){ //对每个元素右边所有的元素逐个比较，确定出其中最小的值
                 if (less(a[j], a[min])) min=j;
-            exch(a,i,min);
             }
-
+            exch(a,i,min); //exch在for循环外部
         }
+    }
+
+    public static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w)<0;
+    }
+
+    public static void exch(Comparable[] a , int i ,int j){
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
     }
 
     public static void main(String[] args) {
         Integer[] a = {19,5,4,3,8,7,10};
         sort(a);
-        for (int s : a){
-            System.out.println(s);
-        }
-
-
+       for (int k=0; k<a.length; k++){
+           System.out.println(a[k]);
+       }
     }
 }
